ac70778f01db0733c107fe4aead5c55484974557
KarishmaSharma24 <86296751+KarishmaSharma24@users.noreply.github.com>
2024-10-07 09:29:37 +0530

    add product-list

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MainLayout from './Components/MainLayout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Contact from './Components/Contact';
 import { BreadcromProvider } from './hooks/ContextAPI/Breadcrum';
+import ProductHome from './Components/ProductList';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<MainLayout content={<Home />} />} />
         <Route path="/about" element={<MainLayout content={<About />}/>} />
         <Route path="/contact" element={<MainLayout content={<Contact/>}/>}/>
+        <Route path="/product-list" element={<MainLayout content={<ProductHome/>} />} />
       </Routes>
     </Router>
     </BreadcromProvider>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,6 +15,7 @@ function Header() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/product-list">Product</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
