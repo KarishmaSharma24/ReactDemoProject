ccb47bff0931966187ffb95a57832eb34573201e
KarishmaSharma24 <86296751+KarishmaSharma24@users.noreply.github.com>
2024-10-26 16:03:29 +0530

    updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,41 +1,78 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
-
 import axios from "axios";
 import React from "react";
 import { useState } from "react";
 
 
 function Login() {
-      // const [email, setEmail] = useState("");
-      // const [password, setPassword] = useState("");
 
-    
+      const [user, setUser] = useState({
+            email: "",
+            password: "",
+      });
 
-      // function handleLoginForm(e){
-      //       e.preventDefault();
-      //       const loginData = {
-      //             email,
-      //             password,
-      //       }
-      //       console.log("lgoin");
-      //       console.log(loginData);
-            
-      // }     
+      const [errors, setErrors] = useState({});
+      const [generalError, setGeneralError] = useState("");
+
+      const validateErrors = (inputFields) => {
+            let errors = {};
+            if (!user.email) {
+                  errors.email = "Email is required";
+            }
+            if (!user.password) {
+                  errors.password = "Password is required";
+            }
+            return errors;
+      }
+
+      function handleChange(e) {
+            e.preventDefault();
+            const { name, value } = e.target;
+            setUser(() => ({
+                  ...user,
+                  [name]: value,
+            }));
+            setErrors(validateErrors({ ...user, [name]: value }));
+      }
+
+      function handleSubmit(e) {
+            e.preventDefault();
+            const validationErrors = validateErrors(user);
+            setErrors(validationErrors);
+
+            if (Object.keys(validationErrors).length > 0) {
+                  alert(Object.values(validationErrors).join('\n'));
+            } else {
+                  axios.post('http://localhost:8000/api/login', user)
+                        .then(response => {
+                              setGeneralError("");
+                              console.log(response);
+                              alert("Login successful");
+                        })
+                        .catch(error => {
+                              setGeneralError("");
+                              setGeneralError("Invalid credentials"+error.error)    ;
+                              console.error("Error:", error);
+                              alert("Error logging in");
+                        });
+            }     
+      }
+     
 
       return (
             <>
             <h1 className="text-center">Login Form</h1>
-                  {/* <Form className="p-5" onSubmit={handleLoginForm}>
-                       
+            {generalError ? <span className="error text-danger">{generalError}</span> : null}
+                  <Form className="p-5" onSubmit={handleSubmit}>
                         <Row className="mb-3 ">
                               <Form.Group as={Col} controlId="formGridEmail">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" name="email" value={user.email} placeholder="Enter email" onChange={(e)=>{e.target.value}} />
+                                    <Form.Control type="email" name="email" onChange={handleChange} isInvalid={!!errors.email} placeholder="Enter email"  />
                               </Form.Group>
 
                               <Form.Group as={Col} controlId="formGridPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" name="password" value={user.password} placeholder="Password" onChange={(e)=>{e.target.value}} />
+                                    <Form.Control type="password" name="password" onChange={handleChange} placeholder="Password" isInvalid={!!errors.email} />
                               </Form.Group>
                         </Row>
 
@@ -43,7 +80,7 @@ function Login() {
                         <Button variant="primary" type="submit" >
                               Login
                         </Button>
-                  </Form> */}
+                  </Form>
             </>
       );
 }
